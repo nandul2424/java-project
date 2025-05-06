c50e2ce8c5ca95981df1f2d599cb0af7805c7e9e
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-05-06 19:32:20 +0530

    Developing interface

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/views/ViewFactoryTravelTools.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/views/ViewFactoryTravelTools.java
@@ -2,10 +2,84 @@ package com.bluelanka_guide.views;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ViewFactoryTravelTools {
+    private AnchorPane travelToolsView;
+    private AnchorPane checklistView;
+    private AnchorPane currencyConverterView;
+    private AnchorPane emergencyContactsView;
+    private AnchorPane unitConverterView;
+    private AnchorPane weatherView;
+
     public ViewFactoryTravelTools(){}
+
+    public AnchorPane getTravelToolsView() {
+        try{
+            if(travelToolsView == null){
+                travelToolsView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/TravelTools.fxml")).load();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return travelToolsView;
+    }
+
+    public AnchorPane getChecklistView() {
+        try{
+            if(checklistView == null){
+                checklistView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/Checklist.fxml")).load();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return checklistView;
+    }
+
+    public AnchorPane getCurrencyConverterView() {
+        try{
+            if(currencyConverterView == null){
+                currencyConverterView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/CurrencyConverter.fxml")).load();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return currencyConverterView;
+    }
+
+    public AnchorPane getEmergencyContactsView() {
+        try{
+            if(emergencyContactsView == null){
+                emergencyContactsView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/EmergencyContacts.fxml")).load();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return emergencyContactsView;
+    }
+
+    public AnchorPane getUnitConverterView() {
+        try{
+            if(unitConverterView == null){
+                unitConverterView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/UnitConverter.fxml")).load();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return unitConverterView;
+    }
+
+    public AnchorPane getWeatherView() {
+        try{
+            if(weatherView == null){
+                weatherView = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/Weather.fxml")).load();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return weatherView;
+    }
 
     public void showTravelToolsWindow(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/TravelToolsPage/TravelTools.fxml"));
