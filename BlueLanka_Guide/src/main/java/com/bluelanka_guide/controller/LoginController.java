99334c282b4510b14de7dbd65eaa2c71effa2ab4
dasunithimasha <dasunithimasha25@gmail.com>
2025-05-10 18:49:55 +0530

    develop interfaces

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/LoginController.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/LoginController.java
@@ -1,4 +1,24 @@
 package com.bluelanka_guide.controller;
 
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private Button loginbtn_id;
+
+    @FXML
+    private PasswordField passwordtext;
+
+    @FXML
+    private Button registerbtn_id;
+
+    @FXML
+    private TextField usernametext;
+
 }
