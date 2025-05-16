c75d989f48242af778ceedb1a34f0f0529875bcd
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-05-16 13:41:55 +0530

    Setting Unit Converter UI

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/TravelToolsPage/UnitConverterController.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/TravelToolsPage/UnitConverterController.java
@@ -1,21 +1,42 @@
 package com.bluelanka_guide.controller.TravelToolsPage;
 
 import com.bluelanka_guide.models.Model;
+import com.bluelanka_guide.models.UnitType;
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UnitConverterController implements Initializable {
     public Button btnBack;
+    public TextField txtFromAmount;
+    public ComboBox cmbFromUnit;
+    public ComboBox cmbToUnit;
+    public Button btnSwap;
+    public Button btnConvert;
+    public Label lblError;
+    public TextField txtToAmount;
+    public ChoiceBox<UnitType> chbSelectUnit;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //back button function
         btnBack.setOnAction(event -> onBackBtnClicked());
+
+        //setting choicebox values
+        chbSelectUnit.setItems(FXCollections.observableArrayList(UnitType.Length, UnitType.Weight, UnitType.Area, UnitType.Temperature, UnitType.Volume, UnitType.Time));
+
+        //convert button function
+        btnConvert.setOnAction(event -> onConvert());
     }
 
     private void onBackBtnClicked() {
         Model.getInstance().getViewFactory().getToolSelectedMenuItem().set("back");
+    }
+
+    private void onConvert() {
+        System.out.println("convert");
     }
 }
--- BlueLanka_Guide/src/main/java/module-info.java
+++ BlueLanka_Guide/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.bluelanka_guide {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires java.net.http;
 
 
     opens com.bluelanka_guide to javafx.fxml;
