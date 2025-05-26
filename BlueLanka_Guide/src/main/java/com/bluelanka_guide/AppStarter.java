6a4161005a0fc61ea95882d047633e4a2bd810e1
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-05-26 20:52:11 +0530

    commit before pulling

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/AppStarter.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/AppStarter.java
@@ -8,15 +8,6 @@ public class AppStarter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-//        FXMLLoader fxmlLoader = new FXMLLoader(AppStarter.class.getResource("/FXML/TravelToolsPage/CurrencyConverter.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        stage.setTitle("Travel Tools");
-//        stage.setScene(scene);
-//        stage.show();
-
-        Model.getInstance().getViewFactoryTravelTools().showTravelToolsWindow();
+        Model.getInstance().getViewFactoryMain().showLoginWindow();
     }
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
 }
--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/views/ViewFactoryMain.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/views/ViewFactoryMain.java
@@ -1,5 +1,56 @@
 package com.bluelanka_guide.views;
 
-public class ViewFactoryMain {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class ViewFactoryMain {
+    private BorderPane loginView;
+    private BorderPane signUpView;
+
+    public BorderPane getLoginView(){
+        try{
+            if(loginView == null){
+                loginView = new FXMLLoader(getClass().getResource("/FXML/Login.fxml")).load();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return loginView;
+    }
+
+    public BorderPane getSignUpView(){
+        try{
+            if(signUpView == null){
+                signUpView = new FXMLLoader(getClass().getResource("/FXML/SignUp.fxml")).load();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return signUpView;
+    }
+
+    public void showLoginWindow(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showDashboardWindow(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Dashboard.fxml"));
+        createStage(loader);
+    }
+
+    public void createStage(FXMLLoader loader){
+        Scene scene = null;
+        try{
+            scene = new Scene(loader.load());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setTitle("BlueLanka Guide");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
