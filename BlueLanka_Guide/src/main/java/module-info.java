49b0ef5e70c7ec411e07095e926d248cf5e1c81b
Jkr200210 <kavishanrandi@gmail.com>
2025-05-27 21:43:10 +0530

    commit befoer pull

--- BlueLanka_Guide/src/main/java/module-info.java
+++ BlueLanka_Guide/src/main/java/module-info.java
@@ -2,9 +2,12 @@ module com.bluelanka_guide {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires javafx.web;
+    requires java.naming;
 
 
     opens com.bluelanka_guide to javafx.fxml;
     opens com.bluelanka_guide.controller.TravelToolsPage to javafx.fxml;
+    exports com.bluelanka_guide.controller.DestinationsPage;
     exports com.bluelanka_guide;
 }
