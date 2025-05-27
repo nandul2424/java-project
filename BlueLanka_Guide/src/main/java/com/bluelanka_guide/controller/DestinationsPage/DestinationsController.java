f736df120ff26f03912dfaca1d0eb01764b59f28
Jkr200210 <kavishanrandi@gmail.com>
2025-05-27 23:33:24 +0530

    destination class create

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/DestinationsPage/DestinationsController.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/DestinationsPage/DestinationsController.java
@@ -16,5 +16,34 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.List;
 
+public class DestinationsController extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
 
+    }
+
+    static class Destination {
+        private final int id;
+        private final String name;
+        private final String country;
+        private final String description;
+        private final double rating;
+        private final String imagePath;
+        private final double latitude;
+        private final double longitude;
+
+        public Destination(int id, String name, String country, String description, double rating, String imagePath, double latitude, double longitude) {
+            this.id = id;
+            this.name = name;
+            this.country = country;
+            this.description = description;
+            this.rating = rating;
+            this.imagePath = imagePath;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+
+    }
+}
 
