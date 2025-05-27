11721f72101d0b116dedea8c39a5e738eeab77f8
Jkr200210 <kavishanrandi@gmail.com>
2025-05-27 22:39:18 +0530

    import other javafx files

---
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/DestinationsPage/DestinationsController.java
@@ -0,0 +1,18 @@
+package com.bluelanka_guide.controller.DestinationsPage;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
