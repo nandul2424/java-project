c85ee840929ca79c964449c75ed06a68dbacaeb4
Jkr200210 <kavishanrandi@gmail.com>
2025-06-01 23:46:03 +0530

    interface modified

--- BlueLanka_Guide/src/main/java/module-info.java
+++ BlueLanka_Guide/src/main/java/module-info.java
@@ -9,13 +9,16 @@ module com.bluelanka_guide {
     requires javafx.web;
     requires java.naming;
     requires annotations;
+    requires jdk.jsobject;
 
 
     opens com.bluelanka_guide to javafx.fxml;
+    opens com.bluelanka_guide.controller.DestinationsPage to javafx.fxml;
     opens com.bluelanka_guide.controller.TravelToolsPage to javafx.fxml;
     opens com.bluelanka_guide.controller.TripPlanner to javafx.fxml;
     opens com.bluelanka_guide.controller to javafx.fxml;
     exports com.bluelanka_guide.controller;
+    exports com.bluelanka_guide.controller.DestinationsPage;
     exports com.bluelanka_guide.controller.TravelToolsPage;
     exports com.bluelanka_guide.controller.TripPlanner;
     exports com.bluelanka_guide;
